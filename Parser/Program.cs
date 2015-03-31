4c678c0c6883b06204d20b3b013125962f763ce5
Glory Lo <gloryl604@gmail.com>
2015-03-31 10:42:54 -0700

    added parse error check

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Dynamic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -34,16 +35,26 @@ namespace Parser
             profile2.Age = 32;
             profile2.Location = "Burnaby";
 
+            
+            var s = @"[Location == ""Vancouver""";
+            try
+            {
+                 var pred = SimpleExpression.PredicateParser<IDictionary<string, object>>.Parse(s);
+                 //var pred = SimpleExpression.PredicateParser<Profile>.Parse(s);
+                 Console.WriteLine("String: {0}", s);
+                 Console.WriteLine("Expr Tree:  {0}", pred.ToString());
+                 var predicateCompiled = pred.Compile();
+                 var matchLocation = predicateCompiled(profile);
+                 //var matchLocation = predicateCompiled(profile2);
+                 Console.WriteLine("Output of prediate: {0}", matchLocation);
 
-            var s = @"[Location] == ""Vancouver""";
-            var pred = SimpleExpression.PredicateParser<IDictionary<string, object>>.Parse(s);
-            //var pred = SimpleExpression.PredicateParser<Profile>.Parse(s);
-            Console.WriteLine("String: {0}", s);
-            Console.WriteLine("Expr Tree:  {0}", pred.ToString());
-            var predicateCompiled = pred.Compile();
-            var matchLocation = predicateCompiled(profile);
-            //var matchLocation = predicateCompiled(profile2);
-            Console.WriteLine("Output of prediate: {0}", matchLocation);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Parse Error: " + e.Message);
+
+            }
+
             Console.ReadLine();
 
 
