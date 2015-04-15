7fbdb364870eeb0bd9626ead8c7e33dacaaa0b16
Glory Lo <gloryl604@gmail.com>
2015-04-15 10:39:42 -0700

    added some dynamic tests (needs retesting)

--- Tests/DynamicObjectTests.cs
+++ Tests/DynamicObjectTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Dynamic;
 using NUnit.Framework;
 
@@ -13,26 +14,79 @@ namespace Tests
         public void BeforeTest()
         {
             Product = new ExpandoObject();
-            Product.Id = "1234567";
-            Product.Description = "The best widget for your household.  Works wonders";
-            Product.Price = "34.99";
-            ((IDictionary<string, object>) Product)["Inventory Status"] = "In Stock";
-            Product.Quantity = 30;
+            var product = Product as IDictionary<string, object>;
+            product["Id"] = "1234567";
+            product["Description"] = "The best widget for your household.  Works wonders";
+            product["Price"] = 34.99;
+            product["Inventory Status"] = "In Stock";
+            product["Quantity"] = 30;
         }
 
         [Test]
-        public void VerifyWhiteSpaceIdentifer()
+        public void VerifyWhiteSpaceEqualsIdentifer()
         {
-            var profile = new ExpandoObject() as IDictionary<string, object>;
-            profile["Username"] = "Johny";
-            profile["Age"] = 33;
-            profile["Location"] = "Vancouver";
-            profile["Online Status"] = "Online Now";
+            var expr = @"[Inventory Status] == ""In Stock""";
+            var product = Product as IDictionary<string, object>;
+            Assert.IsTrue(ExpressionEvaluator.Evaluate(expr, product));
+        }
 
+        [Test]
+        public void VerifyWhiteSpaceNotEqualsIdentifer()
+        {
+            var expr = @"[Inventory Status] != ""Available""";
+            var product = Product as IDictionary<string, object>;
+            Assert.IsTrue(ExpressionEvaluator.Evaluate(expr, product));
+        }
+        
+        [Test]
+        public void VerifyWithEqualsReservedWord()
+        {
+            var expr = @"[Inventory Status] Equals? ""In Stock""";
+            var product = Product as IDictionary<string, object>;
+            Assert.IsTrue(ExpressionEvaluator.Evaluate(expr, product));
+        }
 
-            var expr = @"[Online Status] Equals? ""Online Now""";
-            Assert.IsTrue(ExpressionEvaluator.Evaluate(expr, profile));
+        [Test]
+        public void VerifyContainingReservedWord()
+        {
+            var expr = @"Description Containing? ""best widget""";
+            Assert.IsTrue(ExpressionEvaluator.Evaluate(expr, Product));
+        }
 
+        [Test]
+        public void VerifyNotContainingReservedWord()
+        {
+            var expr = @"!(Description Containing? ""test"")";
+            Assert.IsTrue(ExpressionEvaluator.Evaluate(expr, Product));
+        }
+
+        [Test]
+        public void VerifyStartsWithReservedWord()
+        {
+            var expr = @"Description StartsWith? ""The best""";
+            Assert.IsTrue(ExpressionEvaluator.Evaluate(expr, Product));
+        }
+
+        [Test]
+        public void VerifyNotStartsWithReservedWord()
+        {
+            var expr = @"!(Description StartsWith? ""The test"")";
+            Assert.IsTrue(ExpressionEvaluator.Evaluate(expr, Product));
+        }
+
+        [Test]
+        public void VerifyGetProperty()
+        {
+            var expr = @"Price == 34.99";
+            Assert.IsTrue(ExpressionEvaluator.Evaluate(expr, Product));
+        }
+
+        [Test]
+        public void VerifyLessThanCondition()
+        {
+            var expr = @"Price < 40";
+            dynamic p = Product;
+            Assert.IsTrue(ExpressionEvaluator.Evaluate(expr, p));
         }
 
     }
