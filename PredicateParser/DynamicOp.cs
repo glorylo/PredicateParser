1ac06a2767a15888f64bc592d709abc805bc1239
Glory Lo <gloryl604@gmail.com>
2015-04-15 17:28:04 -0700

    added unaryop

--- PredicateParser/DynamicOp.cs
+++ PredicateParser/DynamicOp.cs
@@ -6,10 +6,21 @@ namespace PredicateParser
 {
     public static class DynamicOp
     {
+        private static Expression UnaryOp(Expression lhs, ExpressionType expressionType)
+        {
+            var expArgs = new List<Expression>() { lhs };
+            var binderM = Binder.UnaryOperation(CSharpBinderFlags.None, expressionType, lhs.Type, new []
+		            {
+			            CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null),
+			            CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null)
+		            });
+            return Expression.Dynamic(binderM, typeof(object), expArgs);
+        }
+
         public static Expression BinaryOp(Expression lhs, Expression rhs, ExpressionType expressionType)
         {
             var expArgs = new List<Expression>() { lhs, rhs };
-            var binderM = Binder.BinaryOperation(CSharpBinderFlags.None, expressionType, lhs.Type, new CSharpArgumentInfo[]
+            var binderM = Binder.BinaryOperation(CSharpBinderFlags.None, expressionType, lhs.Type, new []
 		            {
 			            CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null),
 			            CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null)
