99a8fc59bc260d996805f296bdd0d24159ef0ede
Yannik Henke <yannik.henke@tecalliance.net>
2020-01-28 13:58:23 +0100

    Added possibility to use enums with ToString() method

---
+++ PredicateParser.Tests/EnumTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using PredicateParser.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredicateParser.Tests
+{
+    [TestFixture]
+    public class EnumTests
+    {
+        public dynamic Person;
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            Person = People.John;
+        }
+
+        [Test]
+        public void TestRightEnum()
+        {
+            var expr = "Gender Equals? \"MALE\"";
+            var gender = Person.Gender.ToString();
+            Assert.IsTrue(ExpressionEvaluator.Evaluate(expr, Person));
+        }
+
+        [Test]
+        public void TestWrongEnum()
+        {
+            var expr = "Gender Equals? \"FEMALE\"";
+            Assert.IsFalse(ExpressionEvaluator.Evaluate(expr, Person));
+        }
+    }
+}
